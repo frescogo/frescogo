c58ab0ea77978e3e29c0d5abdf5bf923c17a9ed9
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-03-27 12:57:02 -0300

    ...

--- stats/parse.lua
+++ stats/parse.lua
@@ -1,21 +1,32 @@
 local m = require 'lpeg'
-local P, S, C, R = m.P, m.S, m.C, m.R
+local P, S, C, R, Ct = m.P, m.S, m.C, m.R, m.Ct
 
 local SPC  = S'\t\n\r '
-local SPCS = S'\t\n\r ' ^ 0
+local X    = SPC ^ 0
+local NUMS = R'09' ^ 1
 
-local patt = P'relatorio'^-1                                * SPCS *
-             P'-'^0                                         * SPCS *
-             C((1-SPC)^0)                                   * SPCS *    -- Joao
-             P'/'                                           * SPCS *
-             C((1-SPC)^0)                                   * SPCS *    -- Maria
-             P'(' * C(R'09'^1) * P'cm'                      * SPCS *    -- 750cm
-             P'-'                                           * SPCS *
-             C(R'09'^1) * P's' * P')'                       * SPCS *    -- 180s
-             P'-'^0                                         * SPCS *
-             P'TOTAL:' * SPCS * C(R'09'^1)                  * SPCS *    -- 5000 pontos
-             P'Tempo:' * SPCS * C(R'09'^1) * P'ms (-0s)'    * SPCS *    -- 180650 (-0s)
-             P(0)
+local patt =
+    P'relatorio'^-1                         * X *
+    P'-'^0                                  * X *
+    C((1-SPC)^0)                            * X *   -- Joao
+    P'/'                                    * X *
+    C((1-SPC)^0)                            * X *   -- Maria
+    P'(' * C(NUMS) * P'cm'                  * X *   -- 750cm
+    P'-'                                    * X *
+    C(NUMS) * P's' * P')'                   * X *   -- 180s
+    P'-'^0                                  * X *
+    P'TOTAL:'  * X * C(NUMS)                * X *   -- 3701 pontos
+    P'Tempo:'  * X * C(NUMS) * P'ms (-0s)'  * X *   -- 180650 (-0s)
+    P'Quedas:' * X * C(NUMS)                * X *   -- 6 quedas
+    P'Golpes:' * X * C(NUMS)                * X *   -- 286 golpes
+    P'Ritmo:'  * X * C(NUMS)                * X *   -- 45 kmh
+    (1-NUMS)^1 * C(NUMS)                    * X *   -- Joao: 5500
+    P'[' * Ct((X * C(NUMS))^1) * X * P']'   * X *   -- [ ... ]
+    P'[' * Ct((X * C(NUMS))^1) * X * P']'   * X *   -- [ ... ]
+    (1-NUMS)^1 * C(NUMS)                    * X *   -- Maria: 4427
+    P'[' * Ct((X * C(NUMS))^1) * X * P']'   * X *   -- [ ... ]
+    P'[' * Ct((X * C(NUMS))^1) * X * P']'   * X *   -- [ ... ]
+    P(0)
              
 
 print(patt:match(assert(io.open(...)):read'*a'))
