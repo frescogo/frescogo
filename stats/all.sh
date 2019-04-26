0d8194de6bf0fd93ef14303bd1af6cf563a0a78b
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-04-26 12:46:48 -0300

    (+) stats script

---
+++ stats/all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# ./all.sh ../Jogos/20190405/
+
+for i in $1/serial_*.txt; do
+    echo $i
+    base=`basename $i .txt`
+    lua5.3  parse.lua    $i              /tmp
+    python3 histogram.py "/tmp/$base.py" $1
+done
