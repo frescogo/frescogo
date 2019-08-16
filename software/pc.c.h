032d289b1f26be15f25d1be4ac6b84f73def8bd8
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-08-16 00:32:51 -0300

    Pequeno ajuste.

--- software/pc.c.h
+++ software/pc.c.h
@@ -59,7 +59,7 @@ void PC_Tick (void) {
     Serial.print(F(";"));
     Serial.print(G.hits);             // total de golpes
     Serial.print(F(";"));
-    Serial.print(G.pace[0]);          // media simples
+    Serial.print((int)G.pace[0]);     // media simples
     Serial.print(F(";"));
     Serial.println();
 }
