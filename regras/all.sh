091b60d24deabf7d43f6801d48d8ee9f2845fe1b
Francisco Sant'Anna <francisco.santanna@gmail.com>
2020-01-29 19:16:15 -0300

    Texto da regra

--- regras/all.sh
+++ regras/all.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
-pandoc regra-150.md -H deeplists.tex -o /tmp/x.pdf
-pdftoppm /tmp/x.pdf /tmp/x -png
-convert /tmp/x-1.png -trim /tmp/x.png
-convert /tmp/x.png -bordercolor White -border 8 regra-150.png
+#pandoc regra-150.md -H deeplists.tex -o /tmp/x.pdf
+#pdftoppm /tmp/x.pdf /tmp/x -png
+#convert /tmp/x-1.png -trim /tmp/x.png
+#convert /tmp/x.png -bordercolor White -border 8 regra-150.png
 #eog regra-150.png
 
-pandoc regra-300.md -H deeplists.tex -o /tmp/x.pdf
-pdftoppm /tmp/x.pdf /tmp/x -png
-convert /tmp/x-1.png -trim /tmp/x.png
-convert /tmp/x.png -bordercolor White -border 8 regra-300.png
+#pandoc regra-240.md -H deeplists.tex -o /tmp/x.pdf
+#pdftoppm /tmp/x.pdf /tmp/x -png
+#convert /tmp/x-1.png -trim /tmp/x.png
+#convert /tmp/x.png -bordercolor White -border 8 regra-240.png
 #eog regra-300.png
 
-pandoc regra-150-en.md -H deeplists.tex -o /tmp/x.pdf
-pdftoppm /tmp/x.pdf /tmp/x -png
-convert /tmp/x-1.png -trim /tmp/x.png
-convert /tmp/x.png -bordercolor White -border 8 regra-150-en.png
+#pandoc regra-150-en.md -H deeplists.tex -o /tmp/x.pdf
+#pdftoppm /tmp/x.pdf /tmp/x -png
+#convert /tmp/x-1.png -trim /tmp/x.png
+#convert /tmp/x.png -bordercolor White -border 8 regra-150-en.png
 #eog regra-150-en.png
 
-pandoc regra-300-en.md -H deeplists.tex -o /tmp/x.pdf
-pdftoppm /tmp/x.pdf /tmp/x -png
-convert /tmp/x-1.png -trim /tmp/x.png
-convert /tmp/x.png -bordercolor White -border 8 regra-300-en.png
+#pandoc regra-300-en.md -H deeplists.tex -o /tmp/x.pdf
+#pdftoppm /tmp/x.pdf /tmp/x -png
+#convert /tmp/x-1.png -trim /tmp/x.png
+#convert /tmp/x.png -bordercolor White -border 8 regra-300-en.png
 #eog regra-300-en.png
 
-pandoc simples-300.md -H deeplists.tex -o /tmp/x.pdf
+pandoc simples-240.md -H deeplists.tex -o /tmp/x.pdf
 pdftoppm /tmp/x.pdf /tmp/x -png
 convert /tmp/x-1.png -trim /tmp/x.png
-convert /tmp/x.png -bordercolor White -border 8 simples-300.png
+convert /tmp/x.png -bordercolor White -border 8 simples-240.png
 #eog simples-300.png
 
-pandoc simples-150.md -H deeplists.tex -o /tmp/x.pdf
-pdftoppm /tmp/x.pdf /tmp/x -png
-convert /tmp/x-1.png -trim /tmp/x.png
-convert /tmp/x.png -bordercolor White -border 8 simples-150.png
+#pandoc simples-150.md -H deeplists.tex -o /tmp/x.pdf
+#pdftoppm /tmp/x.pdf /tmp/x -png
+#convert /tmp/x-1.png -trim /tmp/x.png
+#convert /tmp/x.png -bordercolor White -border 8 simples-150.png
 #eog simples-150.png
--- regras/simples-240.md
+++ regras/simples-240.md
@@ -8,7 +8,7 @@ eog simples-300.png
 
 \pagenumbering{gobble}
 
-## FrescoGO! - Regra para 5:00 minutos
+## FrescoGO! - Regra para 4:00 minutos
 
 Cada atleta é avaliado individualmente em três médias de velocidade:
 
@@ -16,9 +16,9 @@ Cada atleta é avaliado individualmente em três médias de velocidade:
     média de velocidade de todos os golpes (ataques ou defesas).
 - Máximas:
     - Normal:
-        média de velocidade dos `24` golpes mais fortes.
+        média de velocidade dos `20` golpes mais fortes.
     - Revés:
-        média de velocidade dos `16` golpes mais fortes de revés.
+        média de velocidade dos `12` golpes mais fortes de revés.
         - Esse quesito é opcional e pode ser desabilitado.
 
 As três médias constituem a nota de cada atleta com os seguintes pesos:
@@ -31,5 +31,5 @@ A pontuação final da dupla será a média entre as notas dos dois atletas.
 Caso a nota de um dos atletas esteja muito abaixo da média (margem de `5%`),
 então a pontuação final da dupla será essa menor nota.
 
-Cada queda ainda desconta `1%` da pontuação final.
-Com 20 quedas, a apresentação é encerrada.
+Cada queda ainda desconta `1.5%` da pontuação final.
+Com `16` quedas, a apresentação é encerrada.
