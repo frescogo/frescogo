36442132b2754fa21d275b072b15ddba04d8c133
fsantanna <francisco.santanna@gmail.com>
2020-10-22 22:07:18 -0300

    Reorganização dos diretórios.

--- calendario/all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pandoc calendario.md -V geometry:margin=0.7in -V fontsize:11pt -s -o calendario.pdf
-pandoc nacional.md   -V geometry:margin=0.7in -V fontsize:11pt -s -o nacional.pdf
-convert calendario3.png -resize 25% calendario3.pdf
-pdfunite calendario3.pdf calendario.pdf nacional.pdf all.pdf
